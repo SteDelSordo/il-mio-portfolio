ebbc17886fcea7695a7e1395fc4a4a69c0188496
SteDelSordo <stefano.delsordo.89@gmail.com>
2025-08-02 23:09:04 +0200

    Feat: Layout stabile con tema dark/light e interattività funzionante

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using IlMioPortfolio.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents(); // <-- STEP 1: Aggiunge i servizi per l'interattività
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
@@ -12,16 +15,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode(); // <-- STEP 2: Applica la modalità interattiva a tutta l'app
 
 app.Run();
